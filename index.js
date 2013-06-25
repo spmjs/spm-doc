f6cbd747812b3d5cb8143f3cb7bf308f83ad32c6
afc163 <afc163@gmail.com>
2013-06-25 15:34:57 +0800

    spm doc command

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+var fs = require('fs');
 var nico = require('nico');
+var spawn = require('win-spawn');
+var DOC_PATH = '_site';
+var pkg = JSON.parse(fs.readFileSync('./package.json'));
+
+try {
+  var spm = require('spm');
+} catch (e) {
+  console.log(' You need install spm first');
+  process.exit(2);
+}
+var grunt = require('spm-grunt');
+var spmrc = require('spmrc');
+
+module.exports = function(options) {
+  
+  if (options.clean) {
+    spawn('rm', ['-rf', DOC_PATH], { stdio: 'inherit'});
+  }
+
+  if (options.build) {
+    
+  }
+
+  if (options.server) {}
+
+  if (options.watch) {}
+
+  if (options.publish) {}
+
+};
