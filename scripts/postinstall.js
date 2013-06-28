6352616e149d2c0d0e9e86195573c08a626c4ef7
afc163 <afc163@gmail.com>
2013-06-28 11:50:26 +0800

    add postscript for installing spm plugin

---
+++ scripts/postinstall.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+try {
+  var spm = require('spm');
+  spm.plugin.install({
+    name: 'doc',
+    binary: 'spm-doc',
+    description: 'a documentation generator.'
+  });
+} catch(e) {
+  console.log('  you need install spm to register the program');
+  console.log();
+  console.log('    \x1b[31m$ npm install spm -g\x1b[39m');
+  console.log();
+  console.log("  if you have installed spm, it maybe you haven't set a NODE_PATH environment variable");
+  console.log();
+}
+
